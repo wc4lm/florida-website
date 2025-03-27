ca55c9ecb55db4cf59154adf62393c809ce3e68f
Shubham <jankijoshi251@gmail.com>
2025-03-27 15:28:18 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
-const withPWA = require("next-pwa")({
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development"
-});
+  const withPWA = require("next-pwa");
 
 module.exports = withPWA({
     images: {
-        domains: ["cdn.discordapp.com"]
+        domains: [ "cdn.discordapp.com" ]
+    },
+    pwa: {
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+        disable: process.env.NODE_ENV == "development"
     }
 });
